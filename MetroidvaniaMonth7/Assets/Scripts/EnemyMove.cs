a90a1e5052f5d5c804f873fc0a3aeaaa6d7938f9
Mzatlin <michael.zatlin@yahoo.com>
2020-02-20 18:05:37 -0800

    Enemy Can now jump based on a set direction, force, and delay.

--- MetroidvaniaMonth7/Assets/Scripts/EnemyMove.cs
+++ MetroidvaniaMonth7/Assets/Scripts/EnemyMove.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     IMove move;
+    float nextJump;
+    EnemyJump jump; //need to add isGrounded to Jump Interface 
     // Start is called before the first frame update
     void Start()
     {
+        jump = GetComponent<EnemyJump>();
         move = GetComponent<IMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        move.MoveX(0.1f);
+        if (jump.isGrounded)
+        {
+            move.MoveX(0.3f);
+        }
+        nextJump = Random.Range(-0.3f, 0.3f);
+
     }
 }
