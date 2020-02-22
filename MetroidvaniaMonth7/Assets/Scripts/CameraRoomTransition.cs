dbf978e9960f7e9cde05919156e385d2eaba02d5
Mzatlin <michael.zatlin@yahoo.com>
2020-02-22 15:30:01 -0800

    Camera is now bound to certain rooms. Transitioning to different rooms will activate a new camera with its own set bounds

---
+++ MetroidvaniaMonth7/Assets/Scripts/CameraRoomTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRoomTransition : MonoBehaviour
+{
+    [SerializeField]
+    GameObject camera;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            camera.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            camera.SetActive(false);
+        }
+    }
+}
