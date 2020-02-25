a4f738d139da1ea8972ec62b6dfda8da0a1586ad
Mzatlin <michael.zatlin@yahoo.com>
2020-02-24 23:14:41 -0800

    Created Basic Title Screen

---
+++ MetroidvaniaMonth7/Assets/LoadNewLevelOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewLevelOnClick : MonoBehaviour
+{
+    [SerializeField]
+    string name;
+
+    public void OnClickNewLevel()
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+}
