a43716de74d437c71931b2b20bf15369efbadce6
Mzatlin <michael.zatlin@yahoo.com>
2020-03-01 23:57:26 -0800

    Implemented Basic NPC Animation. Fixed Various Bugs in Room Test Scene.

---
+++ MetroidvaniaMonth7/Assets/Scripts/NPCInteractAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteractAnimation : MonoBehaviour
+{
+    IInteractable interact;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        interact = GetComponent<IInteractable>();
+        interact.OnInteract += HandleInteraction;
+    }
+
+    private void HandleInteraction()
+    {
+        animator.SetBool("IsInteracting", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interact.IsInteracting)
+        {
+            animator.SetBool("IsHovering", true);
+        }
+        else
+        {
+            animator.SetBool("IsHovering", false);
+        }
+    }
+}
