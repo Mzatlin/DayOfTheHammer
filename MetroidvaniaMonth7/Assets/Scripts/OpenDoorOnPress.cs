c58c2f8f04c424b9fc52dd80458fd06f56bee9fb
Mzatlin <michael.zatlin@yahoo.com>
2020-03-01 17:01:47 -0800

    Implemented Final Door Open Animation

--- MetroidvaniaMonth7/Assets/Scripts/OpenDoorOnPress.cs
+++ MetroidvaniaMonth7/Assets/Scripts/OpenDoorOnPress.cs
@@ -8,20 +8,24 @@ public class OpenDoorOnPress : MonoBehaviour
     [SerializeField]
     GameObject door;
     Animator animator;
+    Collider2D doorCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = door.GetComponent<Animator>();
+        doorCollider = door.GetComponent<Collider2D>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         animator.SetBool("IsOpen", false);
+        doorCollider.enabled = false;
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
         animator.SetBool("IsOpen", true);
+        doorCollider.enabled = true;
     }
 }
