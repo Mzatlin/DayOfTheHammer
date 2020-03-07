9c85877b047dfd78b1b632cb9172a726b54cfe68
Mzatlin <michael.zatlin@yahoo.com>
2020-03-06 23:49:24 -0800

    Hammer Throw now releases a LineRenderer that links from the player to the hammer, representing a chain.

---
+++ MetroidvaniaMonth7/Assets/Scripts/LineRenderToHammer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRenderToHammer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject hammer;
+    LineRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<LineRenderer>();
+        render.useWorldSpace = true;
+        render.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hammer != null && hammer.activeInHierarchy)
+        {
+            RenderToHammer();
+        }
+        else
+        {
+            render.enabled = false;
+        }
+    }
+
+    void RenderToHammer()
+    {
+        render.enabled = true;
+        render.SetPosition(0, transform.position);
+        render.SetPosition(1, hammer.transform.position);
+
+    }
+}
