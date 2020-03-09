8ce16b91003eacaf8213b5f45266e0c25d2ea967
Mzatlin <michael.zatlin@yahoo.com>
2020-03-08 21:16:00 -0700

    Added generic Hit and Player Health sound classes that have custom sound paths that can be swapped in the editor.

---
+++ MetroidvaniaMonth7/Assets/Scripts/PlaySoundOnHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnHit : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string hitPath;
+
+    IHittable hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = GetComponent<IHittable>();
+        hit.OnHit += HandleHit;
+    }
+
+    void HandleHit()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(hitPath, transform.position);
+    }
+
+}
---
+++ MetroidvaniaMonth7/Assets/Scripts/PlayerHealthSoundManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthSoundManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string health1;
+    [FMODUnity.EventRef]
+    public string health2;
+    [FMODUnity.EventRef]
+    public string playerDeathSound;
+
+    IHealth health;
+    IHittable hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = GetComponent<IHittable>();
+        hit.OnHit += HandleHit;
+        health = GetComponent<IHealth>();
+        health.OnDie += HandleDie;
+    }
+
+    private void HandleHit()
+    {
+        if (health.CurrentHealth < 1)
+            return;
+
+        if(health.CurrentHealth >= 2)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(health1, transform.position);
+        }
+        else if(health.CurrentHealth == 1)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(health2, transform.position);
+        }
+    }
+
+    private void HandleDie()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(playerDeathSound, transform.position);
+    }
+
+
+}
