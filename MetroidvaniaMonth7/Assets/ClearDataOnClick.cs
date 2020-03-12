3b6ddcba0d299e30b3e3c31359bc47fb71f9bef6
Mzatlin <michael.zatlin@yahoo.com>
2020-03-11 22:18:17 -0700

    Title Scene Checks if Player has Set a Checkpoint and toggles the "Continue" accordingly. Added Basis for Storyboard Scene.

---
+++ MetroidvaniaMonth7/Assets/ClearDataOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearDataOnClick : MonoBehaviour
+{
+    [SerializeField]
+    SaveSystemSO saveSystem;
+
+    public void OnClickClear()
+    {
+        saveSystem.ClearData();
+    }
+}
---
+++ MetroidvaniaMonth7/Assets/Scripts/SaveSystemSO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SaveSystem")]
+public class SaveSystemSO : ScriptableObject
+{
+    public CheckpointSO checkPoint;
+
+    public bool CheckIfSaved()
+    {
+        if(checkPoint.checkpointLocation == Vector3.zero)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ClearData()
+    {
+        checkPoint.checkpointLocation = Vector3.zero;
+    }
+}
