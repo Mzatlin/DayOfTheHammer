ada528e0324d4cbd0b9915653ebb3f45bfcd9b2e
tichaeltosen <mrosen86@gmail.com>
2020-03-11 17:45:29 -0700

    added a bunch of sounds

---
+++ MetroidvaniaMonth7/Assets/Scripts/AnimationSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSound : MonoBehaviour
+{
+ private void PistonHit ()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Objects/Piston", GetComponent<Transform>().position);
+
+    }
+
+    private void DoorOpen ()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Objects/Door-Open", GetComponent<Transform>().position);
+
+    }
+
+    private void DoorClose()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Objects/Door-Close", GetComponent<Transform>().position);
+
+    }
+}
