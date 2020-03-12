2e1f3279dffe615511b06d4b457ec1a566ec4e9d
Mzatlin <michael.zatlin@yahoo.com>
2020-03-11 19:00:01 -0700

    Implemented PlaySoundOnDeath. Created various important prefabs.

---
+++ MetroidvaniaMonth7/Assets/Scripts/PlaySoundOnDeath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnDeath : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string deathPath;
+    IHealth health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GetComponent<IHealth>();
+        health.OnDie += HandleDie;
+    }
+
+    private void HandleDie()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(deathPath, transform.position);
+    }
+
+}
