5d07147d4aec373d2b348d8282292da1e8bd40c5
Mzatlin <michael.zatlin@yahoo.com>
2020-03-14 15:55:21 -0700

    Created Statkit prefab. Changed Opening dialog. Added more rooms.

--- MetroidvaniaMonth7/Assets/Scripts/UpgradeOnCollect.cs
+++ MetroidvaniaMonth7/Assets/Scripts/UpgradeOnCollect.cs
@@ -10,14 +10,22 @@ public class UpgradeOnCollect : CollectBase
     UpgradeSO upgrade;
     [SerializeField]
     float upgradeDifference = 1f;
+    SpriteRenderer render;
     // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponentInChildren<SpriteRenderer>();
+    }
 
     protected override void HandleCollection()
     {
         base.HandleCollection();
         upgrade.upgradeAmount += upgradeDifference;
-        gameObject.SetActive(false);
         FMODUnity.RuntimeManager.PlayOneShot("event:/Objects/Ability-Collected");
+        if (render != null)
+        {
+            render.enabled = false;
+        }
     }
 
 }
