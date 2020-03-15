f29b52ee07e6775bb934be680f8a6fd586dfd198
Mzatlin <michael.zatlin@yahoo.com>
2020-03-14 17:56:31 -0700

    Enemies will now only appear if the player enters the room they are in. After killing them, they cannot respawn upon reentry unless a timer requirement is met.

---
+++ MetroidvaniaMonth7/Assets/Scripts/SpawnOnEnable.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnEnable : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemy;
+    [SerializeField]
+    float spawnRate = 30f;
+    float timeBeforeSpawn = 0f;
+
+
+    void Start()
+    {
+        if (enemy != null)
+        {
+            health = enemy.GetComponent<IHealth>();
+        }
+    }
+
+
+    IHealth health;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (CanSpawn())
+        {
+            SetupEnemy();
+        }
+    }
+
+
+    void SetupEnemy()
+    {
+        if(enemy != null && health != null)
+        {
+            health.CurrentHealth = health.MaxHealth;
+            health.IsDead = false;
+            enemy.GetComponent<Collider2D>().enabled = true;
+            enemy.GetComponentInChildren<SpriteRenderer>().enabled = true;
+            enemy.transform.position = transform.position;
+            enemy.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    bool CanSpawn()
+    {
+        if (Time.time >= timeBeforeSpawn)
+        {
+            timeBeforeSpawn = Time.time + spawnRate;
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
