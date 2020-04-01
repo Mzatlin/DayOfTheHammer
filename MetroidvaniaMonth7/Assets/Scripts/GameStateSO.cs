80b7319958091962327c4a47186556983d277222
Mzatlin <michael.zatlin@yahoo.com>
2020-03-31 23:53:43 -0700

    Created SO that manages opening cutscene to prevent replaying the same cutscene on respawn.

---
+++ MetroidvaniaMonth7/Assets/Scripts/GameStateSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameState")]
+public class GameStateSO : ScriptableObject
+{
+    public bool isOpeningComplete = false;
+}
---
+++ MetroidvaniaMonth7/Assets/Scripts/SetActiveOnSO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveOnSO : MonoBehaviour
+{
+    [SerializeField]
+    GameStateSO state;
+    IDialogEnd end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (state != null && state.isOpeningComplete)
+        {
+            gameObject.SetActive(false);
+        }
+        end = GetComponent<IDialogEnd>();
+        end.OnDialogEnd += HandleDialogEnd;
+    }
+
+    private void HandleDialogEnd()
+    {
+        state.isOpeningComplete = true;
+    }
+}
